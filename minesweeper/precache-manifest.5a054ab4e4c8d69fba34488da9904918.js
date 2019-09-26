a5c441f4e2861f484495d68662a2b277df11236f
Denis Maliyshock <maliy-shock@yandex.ru>
2019-09-27 00:05:07 +0300

    minesweeper added

---
+++ minesweeper/precache-manifest.5a054ab4e4c8d69fba34488da9904918.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d3146f2e761a94433391c978036716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc7d5c35cfa18f54175",
+    "url": "/static/css/main.a5d8a3de.chunk.css"
+  },
+  {
+    "revision": "bbfbe0773a75e534e228",
+    "url": "/static/js/2.d21467b0.chunk.js"
+  },
+  {
+    "revision": "7bc7d5c35cfa18f54175",
+    "url": "/static/js/main.c49c36bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
