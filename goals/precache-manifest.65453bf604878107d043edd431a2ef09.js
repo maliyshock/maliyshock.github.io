4fa450df6f0f19f1ac106ac4b57d6c71bf7f2386
Denis Maliyshock <maliy-shock@yandex.ru>
2019-09-27 02:50:00 +0300

    goals added

---
+++ goals/precache-manifest.65453bf604878107d043edd431a2ef09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9a4d6ff100769e06a4d",
+    "url": "/static/css/main.eaa58ea2.chunk.css"
+  },
+  {
+    "revision": "e9a4d6ff100769e06a4d",
+    "url": "/static/js/main.e9a4d6ff.chunk.js"
+  },
+  {
+    "revision": "b54719de399c127e4766",
+    "url": "/static/js/1.b54719de.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e5334fc7815f1d4eafe0d7ca43be3bb",
+    "url": "/index.html"
+  }
+];
